db9660fef42b340189943454f3bee5e5f0c6189a
Ayaan-Mushtaque-Ahmed <ayaan.m.ahmed666@gmail.com>
2026-02-26 10:58:11 +0530

    UC1: Welcome message

---
+++ src/UC1PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class UC1PalindromeCheckerApp {public class UC1_WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+
+        }
+    }
+}
