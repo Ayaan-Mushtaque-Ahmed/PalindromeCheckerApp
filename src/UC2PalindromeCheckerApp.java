48e28bd3b7df73873acfd620ec197dfcee1dc1dd
Ayaan-Mushtaque-Ahmed <ayaan.m.ahmed666@gmail.com>
2026-02-26 11:08:02 +0530

    UC2:Hardcoded palindrome

---
+++ src/UC2PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class UC2PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String word = "madam";
+        System.out.println("Input text: " + word);
+        System.out.println("Is it a Palindrome ? : true ");
+        }
+    }
+
